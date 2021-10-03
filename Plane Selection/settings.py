91534cd10716a2ff37db4815ea0678bf7ac3eee2
RePozitive <Extayle.SM@yandex.ru>
2021-10-03 23:56:01 +0300

    Seceret mongourl

--- Plane Selection/settings.py
+++ Plane Selection/settings.py
@@ -1,6 +1,6 @@
 class Config(object):
     DEBUG = False
-    MONGO_URI = "mongodb+srv://RePozitive:Senkin1965@cluster0.euqll.mongodb.net/Project_planes?retryWrites=true&w=majority"
+    MONGO_URI = "Your mongorurl here"
     MONGODB_SETTINGS = {
         "db": "Planes_project",
         "host": MONGO_URI,
